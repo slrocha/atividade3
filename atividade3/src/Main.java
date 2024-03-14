84e14472522ca4c25069366204bed02733f13f57
slrocha <slrocha17@gmail.com>
2024-03-14 08:55:31 -0300

    ~Questão 2

--- atividade3/src/Main.java
+++ atividade3/src/Main.java
@@ -7,10 +7,21 @@ public class Main {
         Scanner scannner = new Scanner(System.in);
 
         /*1 -Crie um programa que realize uma contagem regressiva de 10 até 1,
-        mostrando cada número na tela.*/
+        mostrando cada número na tela.
 
         for(int i=10; i >= 1; i--){
             System.out.println("Contagem regressiva... " +i);
+        }*/
+
+        /*2 - Escreva um programa que solicite um número ao usuário e exiba a
+        tabuada desse número de 1 a 10.*/
+
+        System.out.println("Digite um número: ");
+        double number = scannner.nextDouble();
+
+        System.out.println("Tabuada do número " +number);
+        for(int i=1; i <=10; i++){
+            System.out.println(number + " x " +i + " = " + (number*i));
         }
     }
 }
