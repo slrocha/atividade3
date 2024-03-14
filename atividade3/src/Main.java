34af3983d144638c9a710110eb18a4a3bd93e76c
slrocha <slrocha17@gmail.com>
2024-03-14 08:39:54 -0300

    Questão 1

---
+++ atividade3/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scannner = new Scanner(System.in);
+
+        /*1 -Crie um programa que realize uma contagem regressiva de 10 até 1,
+        mostrando cada número na tela.*/
+
+        for(int i=10; i >= 1; i--){
+            System.out.println("Contagem regressiva... " +i);
+        }
+    }
+}
