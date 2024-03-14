01b872449f16e73d416f372cdd022c08bf52fa62
slrocha <slrocha17@gmail.com>
2024-03-14 09:19:00 -0300

    Questão 3

--- atividade3/src/Main.java
+++ atividade3/src/Main.java
@@ -14,14 +14,24 @@ public class Main {
         }*/
 
         /*2 - Escreva um programa que solicite um número ao usuário e exiba a
-        tabuada desse número de 1 a 10.*/
+        tabuada desse número de 1 a 10.
 
         System.out.println("Digite um número: ");
-        double number = scannner.nextDouble();
+        double number = scannner.nextDouble(); //considerando qualquer número, inclusive número reais.
 
         System.out.println("Tabuada do número " +number);
         for(int i=1; i <=10; i++){
             System.out.println(number + " x " +i + " = " + (number*i));
+        }*/
+
+        /*3 - Desenvolva um algoritmo que calcule a soma dos números pares de 1 a 100.*/
+
+        int par = 0;
+        for(int i = 1; i <=100; i++){
+            if(i%2 == 0){
+                par = par+i;
+            }
         }
+        System.out.println("A soma dos números pares de 1 à 100 equivale à " +par);
     }
 }
